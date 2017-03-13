1e8feebc50f240e046bcdedc427c5a26ca9a4d44
ethereal-sandeep <ethereal.sandeep@gmail.com>
2017-03-14 03:09:25 +0530

    exception handling

--- rev_hash.py
+++ rev_hash.py
@@ -6,8 +6,13 @@ import sys
 
 
 def main(argv):
-   # hash(sys.argv[1])
-   rev_hash(sys.argv[1]) # take first argument as hash value to operate on
+    try:
+        arg = sys.argv[1]
+    except:
+        print "Error: No argument supplied. usage:$ python rev_hash.py <hash_value> "
+        return
+    # hash(sys.argv[1])
+    rev_hash(sys.argv[1]) # take first argument as hash value to operate on
 
 # from question
 def hash(s):
@@ -19,7 +24,12 @@ def hash(s):
 
 # required solution
 def rev_hash(h):
-    h = int(h)
+    try:
+        h = int(h)
+    except ValueError:
+        print("That's not an int!  example:$ python rev_hash.py 930846109532517 ")
+        return
+
     # list to keep appending the letter (backward fashion),
     # finally we just reverse print the list to get string
     list = []
@@ -27,7 +37,11 @@ def rev_hash(h):
     while h is not 7:
         ix_ll = h % 37 # index of last letter
         h = (h - ix_ll)/37
-        list.append(letters[ix_ll])
+        try:
+            list.append(letters[ix_ll])
+        except:
+            print "invalid input hash_value"
+            return
     string = ""
     for i in reversed(list):
         string+= i
